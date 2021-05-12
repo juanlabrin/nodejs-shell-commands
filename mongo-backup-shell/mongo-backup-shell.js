9d39c3976efc438b5b99faaf37326e610c4ddac5
Juan Paulo Labrin <juanlabrin@gmail.com>
2021-05-12 17:36:02 -0400

    Adding options

--- mongo-backup-shell/mongo-backup-shell.js
+++ mongo-backup-shell/mongo-backup-shell.js
@@ -1,11 +1,43 @@
-// Command: mongodump --archive=dump/demoMetalurgica.20210430.gz --gzip --db=demoMetalurgica
 exports.backupDB = function(args){
-
     console.log('Mongo Backup Shell');
-    console.log(args);
+    const { spawn } = require('child_process');    
+    let options = [];
 
-    const { spawn } = require('child_process');
-    const mongodump = spawn('mongodump', ['--archive=./backup/', '--gzip', '--db='+args.db]);
+    if (Object.entries(args).length === 0 || args.db === '' || args.db === undefined){
+        console.log('Database name is required!, Proccess exit.');
+        process.exit(1);
+    }
+
+    if (Object.entries(args).length > 1){
+        if(args.access != undefined){
+            if(Object.entries(args.access).length === 0 || Object.entries(args.access).length < 5 || args.access === ''){
+                console.log('All access parameters are required!, Proccess exit.');
+                process.exit(1);
+            }
+            Object.keys(args.access).forEach(key => {
+                if(args.access[key] === ''){
+                    console.log(key, 'is required!, Proccess exit.');
+                    process.exit(1);
+                }                
+            });
+            options.push(`--host=${args.access.host}`);
+            options.push(`--port=${args.access.port}`);
+            options.push(`--user=${args.access.user}`);
+            options.push(`--password=${args.access.pass}`);
+            options.push(`--authenticationDatabase=${args.access.authDB}`);
+        }
+        if(args.filename != undefined || args.filename != ''){
+            options.push(`--archive=${args.filename}`);
+        }
+        if(args.compress){
+            options.push(`--gzip`);
+        }
+        options.push(`--db=${args.db}`);
+    } else {
+        options.push(`--db=${args.db}`);
+    }
+    
+    const mongodump = spawn('mongodump', options);
 
     mongodump.on('exit', function(code, signal){
         if(code){
--- test/test-backup.js
+++ test/test-backup.js
@@ -1,8 +1,16 @@
 var mongoBackupShell = require('../mongo-backup-shell/mongo-backup-shell');
+
 mongoBackupShell.backupDB(
     {
-        db: 'demoMetalurgica',
-        user: 'Admin',
-        pass: '123'
+        db: 'demoMetalurgica', // required
+        // access: {
+        //     host: '127.0.0.1', // required
+        //     user: 'admin', // required
+        //     pass: '123', // required
+        //     authDB: 'admin', // required
+        //     port: '27017' // required
+        // }, // optional
+        filename: 'fileName', // optional     
+        compress: true // optional compress --gzip archive
     }
 );
